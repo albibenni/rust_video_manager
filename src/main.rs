af741da01a038ee7a498489bff569fe6f9443c78
Alberto Benatti <albi.benni8@gmail.com>
2025-03-24 17:27:30 +0100

    from base thread

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
+    dotenv::dotenv().ok();
+    let key: &str = "HOST";
+
     println!("Hello, world!");
+
+    let port = match std::env::var(key) {
+        Ok(val) => val,
+        Err(e) => {
+            println!("Error {}: {}", key, e);
+            "localhost:8081".to_string()
+        }
+    };
+    let listener = std::net::TcpListener::bind(port).expect("Something went wrong");
+    for stream in listener.incoming() {
+        println!("Connection established!");
+        println!("{:?}", stream);
+    }
 }
