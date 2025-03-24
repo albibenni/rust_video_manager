cc208462cf5e63e4250a264c5335dac8333db509
Alberto Benatti <albi.benni8@gmail.com>
2025-03-24 21:29:51 +0100

    handling request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::BufRead;
+
 fn main() {
     dotenv::dotenv().ok();
     let key: &str = "HOST";
@@ -14,6 +16,16 @@ fn main() {
     let listener = std::net::TcpListener::bind(port).expect("Something went wrong");
     for stream in listener.incoming() {
         println!("Connection established!");
-        println!("{:?}", stream);
+        handle_connection(stream.expect("Something went wrong"));
     }
+}
+
+fn handle_connection(stream: std::net::TcpStream) {
+    let buf_reader = std::io::BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.expect("nope"))
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("Request: {http_request:#?}");
 }
