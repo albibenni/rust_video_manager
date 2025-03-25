22ac5d8444268daa151856515d1d6d576a0a83a5
Alberto Benatti <albi.benni8@gmail.com>
2025-03-25 15:24:05 +0100

    yep nice base server get serving

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::BufRead;
+use std::io::{BufRead, Write};
 
 fn main() {
     dotenv::dotenv().ok();
@@ -17,15 +17,33 @@ fn main() {
     for stream in listener.incoming() {
         println!("Connection established!");
         handle_connection(stream.expect("Something went wrong"));
+        //println!("{:?}", stream);
     }
 }
 
-fn handle_connection(stream: std::net::TcpStream) {
+fn handle_connection(mut stream: std::net::TcpStream) {
     let buf_reader = std::io::BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.expect("nope"))
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+    let request_line = buf_reader
         .lines()
-        .map(|result| result.expect("nope"))
-        .take_while(|line| !line.is_empty())
-        .collect();
-    println!("Request: {http_request:#?}");
+        .next()
+        .expect("Line not found")
+        .expect("Something went wrong");
+    //println!("Request: {http_request:#?}");
+    // GET
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = std::fs::read_to_string("index.html").expect("file not found");
+        let length = contents.len();
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream
+            .write_all(response.as_bytes())
+            .expect("Something went wrong")
+    } else {
+    }
 }
